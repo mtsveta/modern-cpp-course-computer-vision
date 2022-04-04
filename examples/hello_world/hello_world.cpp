480dae23b225a4855ac62522efda940dab8ce9d9
Svetlana Kyas <mtsveta@gmail.com>
2022-04-04 10:53:21 +0200

    Add material on lecture Week 0: redirecting output to flow; preporseccing, compilation, linking stages

--- examples/hello_world/hello_world.cpp
+++ examples/hello_world/hello_world.cpp
@@ -4,6 +4,7 @@ int main()
 {
     int my_var = 0;
 
+    // 0, 1, 2 corresponds to the redirection of the flow stdin, stdout, std, stderr flow.
     std :: cin >> my_var; // 0
     std :: cout << "number = " << my_var << std :: endl; // 1
     std :: cerr << "boring error message" << std :: endl; // 2
