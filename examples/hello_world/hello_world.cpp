ad3e8a8a5bf7046b622d7cd1bbfc930fe2644d7d
Svetlana Kyas <mtsveta@gmail.com>
2022-04-04 10:50:12 +0200

    Update gitignore

---
+++ examples/hello_world/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int my_var = 0;
+
+    std :: cin >> my_var; // 0
+    std :: cout << "number = " << my_var << std :: endl; // 1
+    std :: cerr << "boring error message" << std :: endl; // 2
+
+    std::cout << "Hello wolrd!" << std::endl; // 1
+}
